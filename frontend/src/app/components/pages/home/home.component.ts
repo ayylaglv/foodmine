3fa8b5d7455d2afa17f392978883e950dbe69068
ayylaglv <gulievaa.y@gmail.com>
2024-10-30 20:30:59 +0100

    added search bar

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Food } from '../../../shared/models/food';
 import { FoodService } from '../../../service/food.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,16 @@ import { FoodService } from '../../../service/food.service';
 })
 export class HomeComponent {
   foods: Food[] = [];
-  constructor(private foodService: FoodService) {
-    this.foods = this.foodService.getAll();
+  constructor(
+    private foodService: FoodService,
+    activatedRoute: ActivatedRoute
+  ) {
+    activatedRoute.params.subscribe((params) => {
+      if (params.searchTerm) {
+        this.foods = this.foodService.getAlFoodsBySearchTerm(params.searchTerm);
+      } else {
+        this.foods = this.foodService.getAll();
+      }
+    });
   }
 }
